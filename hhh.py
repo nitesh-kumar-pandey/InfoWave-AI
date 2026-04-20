92bdc88760d508b3dd5955cb2f072302427e43bd
Nitesh Kumar Pandey <129722127+nitesh-kumar-pandey@users.noreply.github.com>
2026-04-20 13:49:08 +0530

    Update hhh.py

--- hhh.py
+++ hhh.py
@@ -13,7 +13,7 @@ from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from langchain_groq import ChatGroq
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain, LLMChain
